a0dfaa1233a89a838719423aae32c30b6338d097
Dave Arneson <urvaius@gmail.com>
2023-06-06 12:30:26 -0700

    moved page to main

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,9 +10,29 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+
+    async void switcher_Toggled(object sender, ToggledEventArgs e)
     {
-        App.Current.MainPage = new NavigationPage(new SwitchDemoPage());
+        try
+        {
+            if (e.Value == true)
+            {
+                await Flashlight.Default.TurnOnAsync();
+                label.Text = String.Format("Moonstone is On");
+
+            }
+            else
+            {
+                await Flashlight.Default.TurnOffAsync();
+                label.Text = String.Format("Moonstone is Off");
+            }
+            // label.Text = String.Format(" is now {0}", e.Value);
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
     }
     private void OnjgClicked(object sender, EventArgs e)
     {
